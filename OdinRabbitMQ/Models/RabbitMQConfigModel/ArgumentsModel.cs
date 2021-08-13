bfb77e455b844fc1b471c8558e7acc9206d2de75
‘odinsam’ <123679@qq.com>
2021-08-13 18:32:29 +0800

    🦄 refactor(*): remove rabbitmq to OdinMQ

--- OdinRabbitMQ/Models/RabbitMQConfigModel/ArgumentsModel.cs
+++
@@ -1,8 +0,0 @@
-namespace OdinPlugs.OdinNoSql.OdinRabbitMQ.Models.RabbitMQConfigModel
-{
-    public class ArgumentsModel
-    {
-        public string KeyName { get; set; }
-        public string Value { get; set; }
-    }
-}
